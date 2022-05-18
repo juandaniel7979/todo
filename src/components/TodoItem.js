9b34d657f15614102a13f9679d159e6c2797ac4a
Juan Daniel Vargas <juan.daniel7979@gmail.com>
2022-05-17 20:26:54 -0500

    aplicacion ces4 - To Do

---
+++ src/components/TodoItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {FaCheckCircle,FaEdit,FaTrash} from "react-icons/fa";
+
+const TodoItem = ({todo,handleDelete,completed,setEdit}) => {
+    return (
+    <li className='list'>
+        <div className={`list-item ${todo.completed ? "complete": ""}`}>{todo.title}</div>
+        <div>
+            <button className='button-complete' onClick={()=>completed(todo)}>
+                <FaCheckCircle/>
+                </button> 
+            <button className='button-edit' onClick={()=>setEdit(todo)}>
+                    <FaEdit/>
+                </button>
+            <button className='button-delete' onClick={()=>handleDelete(todo)}>
+            <FaTrash/>
+            </button>
+        </div>
+    </li>
+    );
+};
+
+export default TodoItem;
